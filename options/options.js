cdc33c5c8bc15070e2103bc95a407e16c0ccc47a
Crowdbotics <team@crowdbotics.com>
2024-01-08 20:56:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "venturecapitall-45323",
+  url: "https://venturecapitall-45323.botics.co",
+  api: "https://venturecapitall-45323.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
